82a1676fd094c926de136d4f9c9f3f8a095f4c5f
Matthew Damiano <58225683+matthewdamiano66@users.noreply.github.com>
2022-02-01 18:29:06 -0500

    Add files via upload

--- printshows.py
+++ printshows.py
@@ -1,14 +1,28 @@
 import requests
 import secrets
 def main():
-
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt7462410"
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
+    sourceFile = open('data.txt','w')
+    sourceFile2 = open('new.txt','w')
+    showdata = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
+    results = requests.get(showdata)
+   #brings in the initial data to the data file
     data = results.json()
-    print(data)
-# Press the green button in the gutter to run the script.
+    print(data,file = sourceFile)
+    with open('data.txt') as f:
+        #reads in the file and then, formats them horizontally
+        lines = f.readlines()
+        print(lines,file = sourceFile2)
+        punc = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        print (punc)
+
+        def remove_punc(string):
+            punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~'''
+            for ele in data:
+                if ele in punc:
+                    string = string.replace(ele, "")
+            return data.txt
+
+
 if __name__ == '__main__':
     main()
+
