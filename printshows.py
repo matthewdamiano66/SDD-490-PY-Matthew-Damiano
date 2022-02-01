5eaaa320770bfc415ec82553f963a31965784414
mattdamiano <matthewdamiano66@gmail.com>
2022-02-01 09:53:44 -0500

    Removed not working file items

--- printshows.py
+++ printshows.py
@@ -1,14 +1,10 @@
 import requests
 import secrets
 def main():
-
-    loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt7462410"
-    results = requests.get(loc)
-    if results.status_code != 200:
-        print("help!")
-        return
+    showdata = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
+    results = requests.get(showdata)
     data = results.json()
     print(data)
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+
