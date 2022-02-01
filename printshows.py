4ff7035d1c653e94a83a620adb197ad147a29724
mattdamiano <matthewdamiano66@gmail.com>
2022-02-01 17:54:12 -0500

    Fixced inabillity to write to a file

--- printshows.py
+++ printshows.py
@@ -1,10 +1,11 @@
 import requests
 import secrets
 def main():
+    sourceFile = open('data.txt','w')
     showdata = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
     results = requests.get(showdata)
     data = results.json()
-    print(data)
+    print(data,file = sourceFile)
 if __name__ == '__main__':
     main()
 
