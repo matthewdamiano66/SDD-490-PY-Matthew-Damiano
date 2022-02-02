6b64c3bc58a2a82f12541662833762f635a3f1e8
Matthew Damiano <58225683+matthewdamiano66@users.noreply.github.com>
2022-02-02 18:13:51 -0500

    Add files via upload

--- printshows.py
+++ printshows.py
@@ -4,16 +4,13 @@ def main():
     sourceFile = open('data.txt','w')
     showdata = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
     loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt7462410"
-    rtable = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt1375666"
     reference= requests.get(loc)
     results = requests.get(showdata)
-    rt = requests.get(rtable)
-
    #brings in the initial data to the data file
     data = results.json()
     disp = reference.json()
-    rtt= rt.json()
-    print(disp,data,rtt,file=sourceFile)
+
+    print(disp,data,file=sourceFile)
 
     with open('data.txt') as f:
         #reads in the file and then, formats them horizontally
