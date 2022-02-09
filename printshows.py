4fd37aa2d4715766ca9d991472fd1e8fbe8581cc
johns <jsantore@bridgew.edu>
2022-02-09 18:44:27 -0500

    reformat for pep8 add comment to test workflow

--- printshows.py
+++ printshows.py
@@ -1,20 +1,24 @@
 import requests
 import secrets
+
+# comment to test workflow
 def main():
-    sourceFile = open('data.txt','w')
+    sourceFile = open('data.txt', 'w')
     showdata = f"https://imdb-api.com/en/API/Top250TVs/{secrets.secret_key}"
     loc = f"https://imdb-api.com/en/API/UserRatings/{secrets.secret_key}/tt7462410"
-    reference= requests.get(loc)
+    reference = requests.get(loc)
     results = requests.get(showdata)
-   #brings in the initial data to the data file
+    # brings in the initial data to the data file
     data = results.json()
     disp = reference.json()
 
-    print(disp,data,file=sourceFile)
+    print(disp, data, file=sourceFile)
 
     with open('data.txt') as f:
-        #reads in the file and then, formats them horizontally
+        # reads in the file and then, formats them horizontally
         lines = f.readlines()
-        print(lines,file = sourceFile)
+        print(lines, file=sourceFile)
+
+
 if __name__ == '__main__':
     main()
