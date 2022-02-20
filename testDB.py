a90310c3db53dc9e5c515a45a7a5bbf033cd5f4a
Matthew Damiano <58225683+matthewdamiano66@users.noreply.github.com>
2022-02-20 13:49:43 -0500

    Added a new test for the database
    I have added an additional test for the database so that we are sure it exists and if it exists we print out  a statement.

--- testDB.py
+++ testDB.py
@@ -1,29 +1,30 @@
+import sqlite3
+from sqlite3 import Error
 import dataBaseStuff
-
-# we'll save this one for the next tests
-test_api_data_entry = [{"id": "tttestdata", "rank": "10002", "title": "Comp490 Project 1 Show",
-                        "fullTitle": "Comp490 Project 1 Show (2022)", "year": "2022", "image": "",
-                        "crew": "Prof. Santore and many hardworking students", "imDbRating": "9.2",
-                        "imDbRatingCount": "41"}]
-
-
-# test here actually showed me a hidden error in my database
+test_api_data_entry = [{"id": "this is test data", "rank": "19992", "title": "Comp490 Project 1 Show",
+                        "fullTitle": "Comp490 Project 1(2022)", "year": "2022", "image": "",
+                        "crew": "A lot of people who exist", "imDbRating": "8.8",
+                        "imDbRatingCount": "50"}]
 def test_enter_data():
-    # I should really do some of this in a fixture, but I wanted to do it with just what you already have
-    # the database needs to be deleted everytime to make this test run, which is good for github actions
-    test_data_entry = [("tttestdata", 10002, "Comp490 Project 1 Show", "Comp490 Project 1 Show (2022)", 2022,
-                        "", "Prof. Santore and many hardworking students", 9.2, 41)]
-
+    test_data_entry = [("this is test data", 19992, "Comp490", "Comp490 Project 1 (2022)", 2022,
+                        "", "This is a test for crew", 8.8, 50)]
     connection, db_cursor = dataBaseStuff.open_db("testDatabase.sqlite")
     dataBaseStuff.create_top250_table(db_cursor)
     dataBaseStuff.put_top_250_in_database(test_data_entry, db_cursor)
     connection.commit()
-    # this test in the next four lines wasn't technically required, but I wanted to demo the count feature
-    # and it is a good idea. I could test by checking the len of record_count_set also
     db_cursor.execute("SELECT COUNT() FROM top_show_data WHERE ttid = 'tttestdata'")
     record_count_set = db_cursor.fetchone()
-    number_of_records = record_count_set[0]  # the count returns a tuple, the count is the first element
+    number_of_records = record_count_set[0]
     assert number_of_records == 1
     db_cursor.execute("SELECT * FROM top_show_data WHERE ttid = 'tttestdata'")
     record_set = db_cursor.fetchall()
     assert record_set[0] == test_data_entry[0]
+    def sql_connection():
+        try:
+            con = sqlite3.connect('project1db.sqlite')
+            print("Houston we have lift off ")
+        except Error:
+            print(Error)
+        finally:
+            con.close()
+    sql_connection()
