959b361116244eb1800d73b0615b5f8ff744d912
Matthew Damiano <58225683+matthewdamiano66@users.noreply.github.com>
2022-03-06 14:18:22 -0500

    Updates Gui
    I have updated the gui, not hooked it up to main yet, but It exits and has a button, I am currently working on making my button create a graph displaying the change in ran over time.

--- window.py
+++ window.py
@@ -1,18 +1,28 @@
 
+from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt6.QtGui import QIcon, QFont
 import sys
-from tkinter import Widget
-from PyQt6.QtWidgets import QApplication, QWidget, QPushButton
-
-
-def main():
-    dx =700
-    dy = 500
-    app = QApplication(sys.argv)
-
-    w = QWidget()
-    w.resize(dx, dy)
-    w.setWindowTitle('Sprint 4')
-    w.show()
-    sys.exit(app.exec())
-if __name__ == '__main__':
-    main()
+ 
+class Window(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("IMDB Project 1")
+        self.setGeometry(200,200, 500,500)
+ 
+        self.create_widgets()
+ 
+ 
+    def create_widgets(self):
+        btn = QPushButton("Generate", self)
+        btn.setGeometry(100,100, 200,100)
+        btn.setStyleSheet('background-color:Gray')
+        btn.setIcon(QIcon("football.png"))
+ 
+ 
+ 
+ 
+ 
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec())
