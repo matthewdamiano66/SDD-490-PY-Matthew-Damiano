0a55e18c4d0ff3a79ac2e9aec0f3058f11fe74aa
Matthew Damiano <58225683+matthewdamiano66@users.noreply.github.com>
2022-03-06 18:11:20 -0500

    Guid update-Buttons
    I have added buttons to my gui window describing their eventual function, which will generate a graph depicting show data, both their rise and fall in popularity.

--- window.py
+++ window.py
@@ -13,16 +13,26 @@ class Window(QWidget):
  
  
     def create_widgets(self):
-        btn = QPushButton("Generate", self)
+        btn = QPushButton("Generate Top 250", self)
         btn.setGeometry(100,100, 200,100)
         btn.setStyleSheet('background-color:Gray')
         btn.setIcon(QIcon("football.png"))
  
+        btn2 = QPushButton("Generate Movies",self)
+        btn2.setGeometry(300,300, 400,100)
+        btn2.setStyleSheet('background-color:Gray')
+        btn2.setIcon(QIcon("football.png"))
+        
+        btn3 = QPushButton("Generate Popular Shows",self)
+        btn3.setGeometry(500,500, 400,100)
+        btn3.setStyleSheet('background-color:Gray')
+        btn3.setIcon(QIcon("football.png"))
  
- 
- 
- 
+
 app = QApplication(sys.argv)
 window = Window()
 window.show()
 sys.exit(app.exec())
+def make_graph():
+ print("Window time")
+ hi = make_graph()
