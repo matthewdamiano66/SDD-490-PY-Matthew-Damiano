4a007d78678676b3d5dc4391041e593b13e32c64
Matthew Damiano <58225683+matthewdamiano66@users.noreply.github.com>
2022-03-06 22:50:40 -0500

    Add files via upload

--- window.py
+++ window.py
@@ -7,28 +7,24 @@ class Window(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("IMDB Project 1")
-        self.setGeometry(200,200, 500,500)
- 
+        self.setGeometry(200,200, 1920,1080)
         self.create_widgets()
- 
- 
     def create_widgets(self):
+#button for the generation of the top250 data     
         btn = QPushButton("Generate Top 250", self)
-        btn.setGeometry(100,100, 200,100)
+        btn.setGeometry(100,500, 300,100)
         btn.setStyleSheet('background-color:Gray')
         btn.setIcon(QIcon("football.png"))
- 
+ #button for the generation of the top movies data     
         btn2 = QPushButton("Generate Movies",self)
-        btn2.setGeometry(300,300, 400,100)
+        btn2.setGeometry(500,500, 300,100)
         btn2.setStyleSheet('background-color:Gray')
         btn2.setIcon(QIcon("football.png"))
-        
+#button for the generation of the pop shows data        
         btn3 = QPushButton("Generate Popular Shows",self)
-        btn3.setGeometry(500,500, 400,100)
+        btn3.setGeometry(900,500, 300,100)
         btn3.setStyleSheet('background-color:Gray')
         btn3.setIcon(QIcon("football.png"))
- 
-
 app = QApplication(sys.argv)
 window = Window()
 window.show()
